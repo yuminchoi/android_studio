7ec9f1670a5cbb1c09d33c06f088e1d8004a391a
yuminchoi <chldbals219@naver.com>
2020-09-28 17:06:10 +0900

    나가기버튼+뒤로가기두번눌러종료

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.myapplication;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+
+    private long backKeyPressedTime = 0;
+    private Toast toast;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , SubActivity1.class);
+                startActivity(intent);//activity1 이동
+            }
+        });
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , SubActivity2.class);
+                startActivity(intent);//activity2 이동
+            }
+        });
+        btn3 = findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , SubActivity3.class);
+                startActivity(intent);//activity3 이동
+            }
+        });
+        btn4 = findViewById(R.id.btn4);
+        btn4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("정말로 종료하시겠습니까?");
+                builder.setTitle("종료 알림창")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.setTitle("종료 알림창");
+                alert.show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            toast = Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            finish();
+            toast.cancel();
+        }
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/SubActivity2_2.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SubActivity2_2 extends AppCompatActivity {
+
+
+    ImageView mainImageView;
+    TextView title, description_1;
+
+    String data1, data2;
+
+    int myImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_activity2_2);
+
+        mainImageView = findViewById(R.id.imageView2);
+        title = findViewById(R.id.textView3);
+        description_1 = findViewById(R.id.textView4);
+        description_1.setMovementMethod(new ScrollingMovementMethod());
+
+
+
+        getData();
+        setData();
+    }
+
+
+    private void getData(){
+        if(getIntent().hasExtra("images") && getIntent().hasExtra("description_1")&&
+                getIntent().hasExtra("title"))
+        {
+            data1 = getIntent().getStringExtra("title");
+            data2 = getIntent().getStringExtra("description_1");
+
+            myImage = getIntent().getIntExtra("images", 1);
+
+        } else{
+            Toast.makeText(this,"No data", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private void setData() {
+        title.setText(data1);
+        description_1.setText(data2);
+
+        mainImageView.setImageResource(myImage);
+    }
+}
